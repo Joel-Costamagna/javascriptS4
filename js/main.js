96c56b868357dd6aaab996b1f017b779c33d9f01
Joël COSTAMAGNA <joel.costamagna@protonmail.com>
2017-03-01 11:32:17 +0100

    ça avance

--- js/main.js
+++ js/main.js
@@ -1,44 +1,74 @@
-function main (arguments) {
+const GRILLE_Y = 60;
+const GRILLE_X = 30;
+const grille = [GRILLE_X][GRILLE_Y]; //la grille dans laquelle se déplace le serpent
+var cadre, gamezone, position;
 
-	var cadre = document.getElementById('gamezone');
-	var gamezone = cadre.getContext("2d");
+function main (arguments) {
+	gamezone = cadre.getContext("2d");
+	cadre = document.getElementById('gamezone');
+
+	//le bouton régles affiche les regles ou les cache.
 	document.getElementById('rules').addEventListener('click', function () {
-		alert("c'est trés simple : ");
+		if(document.getElementById('div_rules').style.display == "none"){
+			document.getElementById('div_rules').style.display="block";
+		}else {
+			document.getElementById('div_rules').style.display="none";
+		}
+
 	})
 
-	document.addEventListener('keydown', function(e, gamezone) {
-				bouger(e, gamezone);
+	document.addEventListener('keydown', function(e) {
+				bouger(e);
 	});
 
+	position = [x: 0, y: 0]; //la position de la tête du serpent
 
 }
+
 
 function bouger(event) {
 	switch (event.keyCode) {
 		case 81:
 		case 37:
-			//gauche
-			alert("vous allez à gauche");
+			//alert("vous allez à gauche");
+			if(position.x-1 > 0){
+				position.x--;
+			}
 			break;
 
 		case 90:
 		case 38:
-			//haut
-			alert("vous allez en haut");
+			//alert("vous allez en haut");
+			if (position.y+1 < GRILLE_Y-1) {
+				position.y++;
+			}
 			break;
 
 		case 68:
 		case 39:
-			//droite
-			alert("vous allez à droite");
+			//alert("vous allez à droite");
+			if (position.x+1 < GRILLE_X-1) {
+				position.x++;
+			}
 			break;
 
 		case 83:
 		case 40:
-			//bas
-			alert("vous allez en bas");
+			//alert("vous allez en bas");
+			if (position.y-1 > 0) {
+				position.y--;
+			}
 			break;
 		default:
 			alert("utilisez ZQSD pour se déplacer");
 	}
+}
+
+
+function redraw() {
+
+}
+
+function placerPomme(){
+	var pommeY = math.floor(Math.random() * (GRILLE_Y));
 }
